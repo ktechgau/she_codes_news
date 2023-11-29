8742d6110f4218c3a5c7c11617d8d792ad2b084e
Ktech <ktechgau@gmail.com>
2023-11-29 13:18:50 +0800

    added users/ app, part 2 of projet requirements

--- she_codes_news/news/views.py
+++ she_codes_news/news/views.py
@@ -1,5 +1,7 @@
 from django.views import generic
 from .models import NewsStory
+from django.urls import reverse_lazy
+from .forms import StoryForm
 
 
 class IndexView(generic.ListView):
@@ -12,10 +14,20 @@ class IndexView(generic.ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['latest_stories'] = NewsStory.objects.all()[:4]
+        context['latest_stories'] = NewsStory.objects.all().order_by('-pub_date')[:4]
         return context
     
 class StoryView(generic.DetailView):
     model = NewsStory
     template_name = "news/story.html"
     context_object_name = "story"
+
+class AddStoryView(generic.CreateView):
+    form_class = StoryForm
+    context_object_name = 'news/createStory.html'
+    template_name = 'news/createStory.html'
+    success_url = reverse_lazy('news:index')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super() .form_valid(form)
--- she_codes_news/she_codes_news/settings.py
+++ she_codes_news/she_codes_news/settings.py
@@ -39,7 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users.apps.UsersConfig',
 ]
+
+AUTH_USER_MODEL = 'users.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -124,3 +127,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'news:index'
+LOGOUT_REDIRECT_URL = 'news:index'
+
