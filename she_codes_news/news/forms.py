6a59726987d3514fb08a7884b68a8bb916a1088a
Ktech <ktechgau@gmail.com>
2023-11-29 14:09:37 +0800

    updated to include missing files

---
+++ she_codes_news/news/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from django.forms import ModelForm
+from .models import NewsStory
+
+class StoryForm(ModelForm):
+    class Meta:
+        model = NewsStory
+        fields = ['title', 'pub_date', 'content']
+        widgets = {
+            'pub_date': forms.DateInput (
+                format='%m/%d/%Y',
+                attrs={
+                    'class':'form-control',
+                    'placeholder':'Select a date',
+                    'type':'date'
+                }
+            )
+        }
---
+++ she_codes_news/users/views.py
@@ -0,0 +1,14 @@
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from django.views import generic
+from .models import CustomUser
+from .forms import CustomUserCreationForm
+
+class CreateAccountView(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'users/createAccount.html'
+
+    
+
+# Create your views here.
